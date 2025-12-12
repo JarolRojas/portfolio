f715269256ed04eef270116946999445159f157f
Jarol Rojas <jr.rojas0327@gmail.com>
2025-12-12 03:03:29 +0100

    feat: agregar componente de selección de idioma y lógica de internacionalización

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./shared/components/navbar/navbar";
+import { I18nService } from './core/services/i18n.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  private readonly i18n = inject(I18nService);
   protected readonly title = signal('portfolio');
 }
